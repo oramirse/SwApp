241051baf69cb1c08397dbd6fbfe94eb0b169dcc
Oscar Ramirez <92741022+oramirse@users.noreply.github.com>
2023-12-08 15:43:08 -0500

    Main actualizado

--- SwApp/lib/main.dart
+++ SwApp/lib/main.dart
@@ -5,8 +5,10 @@ import 'package:swapp/pages/home.dart';
 import 'package:swapp/pages/recuperar_contraseña.dart';
 import 'package:swapp/pages/registro.dart';
 import 'package:swapp/pages/login.dart';
-import 'package:swapp/widgets/make_offer.dart';
-import 'package:swapp/widgets/group_schedule_form.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,11 +18,46 @@ void main() async {
   runApp(SwApp());
 }
 
-final key = GlobalKey<GroupScheduleFormState>();
-
 class SwApp extends StatelessWidget {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
   @override
   Widget build(BuildContext context) {
+    _firebaseMessaging.requestPermission();
+    _firebaseMessaging.getToken().then((token) {
+      print("FCM Token: $token");
+    });
+
+    _firebaseMessaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+    final InitializationSettings initializationSettings =
+    InitializationSettings(android: initializationSettingsAndroid);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      print("Received message: ${message.notification?.title}");
+
+      await flutterLocalNotificationsPlugin.show(
+        0,
+        message.notification?.title ?? '',
+        message.notification?.body ?? '',
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            'channel id',
+            'channel name',
+          ),
+        ),
+      );
+    });
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SwApp',
@@ -36,18 +73,15 @@ class SwApp extends StatelessWidget {
             borderSide: BorderSide(width: 1, color: Colors.teal),
           ),
         ),
-        fontFamily: 'SF UI  Text',
+        fontFamily: 'SF UI Text',
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/realizar-oferta',
+      initialRoute: '/login',
       routes: {
-        '/formulario-grupo': (context) => GroupScheduleForm(key: key),
-        '/realizar-oferta': (context) => MakeOffer(userEmail: "ssss"),
         '/recuperar_contraseña': (context) => PasswordRecoveryPage(),
         '/registro': (context) => RegistrationPage(),
         '/login': (context) => LoginPage(),
-        '/home': (context) => HomePage(
-            userEmail: ModalRoute.of(context)?.settings.arguments as String),
+        '/home': (context) => HomePage(userEmail: ModalRoute.of(context)?.settings.arguments as String),
       },
     );
   }
